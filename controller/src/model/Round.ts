885d716654ca77bda2b6da25bfb30e87f3a83790
waffeln <sinusstudios@gmail.com>
2020-04-13 02:19:34 +0200

    Fix typo in round buttons

--- controller/src/model/Round.ts
+++ controller/src/model/Round.ts
@@ -12,7 +12,7 @@ class Round extends RadioItem<RoundType> {}
 const Rounds = {
   midRound: new Round("midRound", "R", "Round", RoundType.midRound),
   quarters: new Round("quarters", "Q", "Quarters", RoundType.quarters),
-  semis: new Round("semis", "Q", "Semis", RoundType.semis),
+  semis: new Round("semis", "S", "Semis", RoundType.semis),
   finals: new Round("finals", "F", "Finals", RoundType.finals)
 } as const
 
